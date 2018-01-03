8f940c14b34556c0c30955e6917b18ed297edb08
Beasley <RBEASLEY@corp.vha.ad>
2018-01-03 15:02:31 -0600

    All the css

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(private RouterModule: Router){ }
+  title = 'app works!';
+
+    
+  ngOnInit() {
+    
+      }
+}
---
+++ example-1/example-1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-example-1',
+  templateUrl: './example-1.component.html',
+  styleUrls: ['./example-1.component.css']
+})
+export class Example1Component implements OnInit {
+
+  constructor(private RouterModule: Router) { }
+
+  ngOnInit() {
+  }
+
+}
